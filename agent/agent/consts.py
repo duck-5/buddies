ca2f1b1dc9acadd32e49516b483c8b5aa303c595
JesusDMan <mantin.work@gmail.com>
2025-11-22 12:38:04 +0200

    Added a basic consts file

---
+++ agent/agent/consts.py
@@ -0,0 +1,2 @@
+EVENTS_FILE_PATH = "events.json"
+LISTS_FILE_PATH = "lists.json"
--- agent/agent/tools/__init__.py
+++ agent/agent/tools/__init__.py
@@ -3,33 +3,27 @@ from agent.tools.list_tools.list_add_item_tool import ListAddTool, ListAddToolCo
 from agent.tools.list_tools.list_remove_item_tool import ListRemoveTool, ListRemoveToolConfig
 from agent.agent.tools.event_tools.add_event_tool import AddEventTool, AddEventToolConfig
 from agent.agent.tools.event_tools.remove_event_tool import RemoveEventTool, RemoveEventToolConfig
-
+from agent.consts import LISTS_FILE_PATH, EVENTS_FILE_PATH
 
 AVAILABLE_TOOLS = [ListAddTool, ListRemoveTool, AddEventTool, RemoveEventTool]
 
 
 TOOLS_CONFIG = {
     ListAddTool.NAME: ListAddToolConfig(
-        list_file_path="lists.json",
-        default_list_name="inbox",
-        max_list_size=5
-    ),
-    
-    ListAddTool.NAME: ListAddToolConfig(
-        list_file_path="lists.json",
+        list_file_path=LISTS_FILE_PATH,
         default_list_name="inbox",
         max_list_size=5
     ),
     ListRemoveTool.NAME: ListRemoveToolConfig(
-        list_file_path="lists.json",
+        list_file_path=LISTS_FILE_PATH,
         allow_audit_logging=True
     ),
 
     AddEventTool.NAME: AddEventToolConfig(
-        event_files_path="events.json"
+        event_files_path=EVENTS_FILE_PATH
     ),
     RemoveEventTool.NAME: RemoveEventToolConfig(
-        events_file_path="events.json"
+        events_file_path=EVENTS_FILE_PATH
     ),
     
 }   
